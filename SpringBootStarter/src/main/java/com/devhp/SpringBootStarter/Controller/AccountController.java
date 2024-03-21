5e3ce3ed85028925e942b32af3a0ee5ebe3ee681
Phuoc <vnhong.phuoc@dai-ichi-life.com.vn>
2024-03-21 14:30:03 +0700

    Add register account form

---
+++ SpringBootStarter/src/main/java/com/devhp/SpringBootStarter/Controller/AccountController.java
@@ -0,0 +1,36 @@
+package com.devhp.SpringBootStarter.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.devhp.SpringBootStarter.models.Account;
+import com.devhp.SpringBootStarter.services.AccountService;
+
+
+
+@Controller
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        Account account = new Account();
+        model.addAttribute("account", account);
+        return "register";
+    }
+
+
+    @PostMapping("/register")
+    public String postMethodName(@ModelAttribute Account account) {
+        accountService.save(account);
+        return "redirect:/";
+    }
+    
+    
+}
