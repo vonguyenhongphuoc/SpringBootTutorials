31fac6c9c8cb1faf5d595c457883ab3269928534
Phuoc <vnhong.phuoc@dai-ichi-life.com.vn>
2024-03-21 11:06:40 +0700

    Add fragments, header, footers to webpages

---
+++ SpringBootStarter/src/main/java/com/devhp/SpringBootStarter/Controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.devhp.SpringBootStarter.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
+    }
+    
+
+}
