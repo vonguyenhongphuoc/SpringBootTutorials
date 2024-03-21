e7012ea4551a8ba7517a96bca8868491512d576d
Phuoc <vnhong.phuoc@dai-ichi-life.com.vn>
2024-03-21 14:01:03 +0700

    Display data on webpage

--- SpringBootStarter/src/main/java/com/devhp/SpringBootStarter/Controller/HomeController.java
+++ SpringBootStarter/src/main/java/com/devhp/SpringBootStarter/Controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.devhp.SpringBootStarter.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.devhp.SpringBootStarter.models.Post;
+import com.devhp.SpringBootStarter.services.PostService;
 
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PostService postService;
+
     @GetMapping("/")
     public String home(Model model) {
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
 
