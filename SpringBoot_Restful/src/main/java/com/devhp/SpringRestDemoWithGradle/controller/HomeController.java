127efe2db1686fdc21605cbb1a812208c3545497
vonguyenhongphuoc <vonguyenhongphuoc93@gmail.com>
2024-03-23 23:30:53 +0700

    Update gradle-wrapper

---
+++ SpringBoot_Restful/src/main/java/com/devhp/SpringRestDemoWithGradle/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.devhp.SpringRestDemoWithGradle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcom to Restful API";
+    }
+    
+}
