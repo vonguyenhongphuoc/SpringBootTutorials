36609fe1400475bd3c8fafb0132f134303265c31
vonguyenhongphuoc <vonguyenhongphuoc93@gmail.com>
2024-03-23 23:09:32 +0700

    Getting started Resful with Gradle

---
+++ SpringBoot_Restful/src/test/java/com/devhp/SpringRestDemoWithGradle/SpringRestDemoWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhp.SpringRestDemoWithGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDemoWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
