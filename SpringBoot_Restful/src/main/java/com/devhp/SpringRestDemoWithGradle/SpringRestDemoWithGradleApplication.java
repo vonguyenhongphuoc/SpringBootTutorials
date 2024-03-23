5948f2cdcd810f1be0ee270244d599fd1e8c7a4b
vonguyenhongphuoc <vonguyenhongphuoc93@gmail.com>
2024-03-23 23:10:52 +0700

    Add a Application Class

---
+++ SpringBoot_Restful/src/main/java/com/devhp/SpringRestDemoWithGradle/SpringRestDemoWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.devhp.SpringRestDemoWithGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDemoWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDemoWithGradleApplication.class, args);
+	}
+
+}
