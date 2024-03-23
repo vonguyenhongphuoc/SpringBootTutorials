1f27226265e5749691938602cd9ea1fe01d51ac1
vonguyenhongphuoc <vonguyenhongphuoc93@gmail.com>
2024-03-23 12:35:29 +0700

    Spring secutiry login, override loadUserByUsername

--- SpringBootStarter_Home/src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
+++ SpringBootStarter_Home/src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
@@ -1,66 +1,62 @@
 package org.studyeasy.SpringStarter.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig {
     private static final String[] WHITELIST = {
-            "/",
-            "/login",
-            "/register",
-            "/db-console/**",
-            "/css/**",
-            "/fonts/**",
-            "/images/**",
-            "/js/**",
+        "/",
+        "/login",
+        "/register",
+        "/db-console/**",
+        "/css/**",
+        "/fonts/**",
+        "/images/**",
+        "/js/**",
 
     };
 
     @Bean
-    public static PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(requests -> {
-            try {
-                requests
-                        .antMatchers(WHITELIST)
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated()
-                        .and()
-                        .formLogin(login -> login
-                                .loginPage("/login")
-                                .loginProcessingUrl("/login")
-                                .usernameParameter("email")
-                                .usernameParameter("password")
-                                .defaultSuccessUrl("/", true).failureUrl("/login?error").permitAll())
-                        .logout(logout -> logout
-                                .logoutUrl("/logout").logoutSuccessUrl("/logout?success"))
-                        .httpBasic(withDefaults());
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        });
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+        .authorizeRequests()
+        .antMatchers(WHITELIST)
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/", true)
+        .failureUrl("/login?error")
+        .permitAll()
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/logout?success")
+        .and()
+        .httpBasic();
 
-        // TODO: remove these after upgrading the DB from H2 infile DB
-        http.csrf(csrf -> csrf.disable());
-        http.headers(headers -> headers.frameOptions().disable());
+        //TODO: remove these after upgrading the DB from H2 infile DB
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
 
         return http.build();
     }
-
+    
 }
--- SpringBootStarter_Home/src/main/java/org/studyeasy/SpringStarter/repositories/AccountRepository.java
+++ SpringBootStarter_Home/src/main/java/org/studyeasy/SpringStarter/repositories/AccountRepository.java
@@ -1,5 +1,7 @@
 package org.studyeasy.SpringStarter.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.studyeasy.SpringStarter.models.Account;
@@ -7,5 +9,6 @@ import org.studyeasy.SpringStarter.models.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
     
+    Optional<Account> findOneByEmailIgnoreCase(String email);
 }
 
