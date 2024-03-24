60e3b7679c686d9e44ecdae7dbc448e77d7d0bda
vonguyenhongphuoc <vonguyenhongphuoc93@gmail.com>
2024-03-24 12:59:53 +0700

    Getting started with secutiry

--- SpringBoot_Restful/src/main/java/com/devhp/SpringRestDemoWithGradle/SpringRestDemoWithGradleApplication.java
+++ SpringBoot_Restful/src/main/java/com/devhp/SpringRestDemoWithGradle/SpringRestDemoWithGradleApplication.java
@@ -3,7 +3,12 @@ package com.devhp.SpringRestDemoWithGradle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @SpringBootApplication
+@SecurityScheme(name = "devhp-demo-api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class SpringRestDemoWithGradleApplication {
 
 	public static void main(String[] args) {
--- SpringBoot_Restful/src/main/java/com/devhp/SpringRestDemoWithGradle/secutiry/SecurityConfig.java
+++ SpringBoot_Restful/src/main/java/com/devhp/SpringRestDemoWithGradle/secutiry/SecurityConfig.java
@@ -4,17 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // @Bean
-    // InMemoryUserDetailsManager users() {
-    //     return new InMemoryUserDetailsManager(
-    //             User.withUsername("devhp").password("{noop}password").authorities("read").build());
-    // }
+    @Bean
+    InMemoryUserDetailsManager users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("devhp").password("{noop}password").authorities("read").build());
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
